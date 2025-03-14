f3065fb3e2fb76a8ffeefecd44f64a1cb2a133c5
Stefane Fermigier <sf@abilian.com>
2025-03-14 18:21:25 +0100

    feat: support for "render()" method.

--- src/flask_fluence/render.py
+++ src/flask_fluence/render.py
@@ -2,5 +2,8 @@ from .templates import get_template
 
 
 def render_component(obj, **kwargs):
+    if render := getattr(obj, "render", None):
+        return render(**kwargs)
+
     template = get_template(obj)
     return template.render(this=obj, **kwargs)
